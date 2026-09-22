6e05486091a42316c83b9a6aedea408439f9fff8
Phama123 <phaphama602@gmail.com>
2026-09-22 14:16:36 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("OB-BOT is alive")
+
+function findOrderBlock() {
+  console.log("Scanning for Order Blocks...);
+  console.log("Bullish OB Found - Ready to trade");
+}
+
+findOrderBlock();
