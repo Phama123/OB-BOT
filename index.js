50c993d44f123ebb87e9e67d05e0ce6b13f540a8
Phama123 <phaphama602@gmail.com>
2026-09-22 16:01:27 +0200

    Set up Express server for OB-BOT

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 10000;
+app.get('/',(reg, res) => res.send('OB-BOT is alive - Scanning for Order Blocks'));
+app.listen(PORT, () => console.log('Web server running on ${PORT}'));
 console.log("OB-BOT is alive")
 
 function findOrderBlock() {
