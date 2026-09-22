3e5b2d57d1ee4b5954c9ea7f983f1de9e1c8965f
Phama123 <phaphama602@gmail.com>
2026-09-22 15:35:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log("OB-BOT is alive")
 
 function findOrderBlock() {
-  console.log("Scanning for Order Blocks...);
+  console.log("Scanning for Order Blocks...");
   console.log("Bullish OB Found - Ready to trade");
 }
 
